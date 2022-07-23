e5374cb3ea7f54b9afd5e8019cbd4495a0fd64a1
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-23 11:55:36 +0300

    authSlice

---
+++ src/features/authSlice.jsx
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: "",
+};
+
+//? createSlice 👉 is a method used to define Redux state logic in a short way.
+//? Slice 👉 The name defines the states as the initial values and the reducers as the key-value structure.
+//? The reducer 👉 provides automatic definition of action types as well as functions that change the state.
+
+const authSlice = createSlice({
+  name: "auth",
+  //! 👇 equal to 👉 initialState: initialState,
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    clearUser: (state, action) => {
+      state.user = "";
+    },
+  },
+});
+
+export const { setUser, clearUser } = authSlice.actions;
+
+export default authSlice.reducer;
