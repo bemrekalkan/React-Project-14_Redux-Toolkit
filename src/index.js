ae439b61cf5400c9c0e9fb8d3c20478fa220e168
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-23 11:14:42 +0300

    version updated with"yarn upgrade --latest" com.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import App from './App';
-import ReactDOM from 'react-dom';
+import App from "./App";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
